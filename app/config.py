0bcd44ebd8d30af111702882ad6b96c396e7b7cd
rico2222 <84716116+rico2222@users.noreply.github.com>
2024-03-11 20:08:30 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ config = {
     "model_weight": "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/hotdog-not-hotdog/data"
                     "/model_weight.pt",
     # TODO: Change it!
-    "project_name": "SAMPLE PROJECT NAME",
+    "project_name": "The Magnificent Hotdog Project",
     # TODO: Change it!
-    "project_description": "SAMPLE PROJECT DESCRIPTION"
+    "project_description": "It's really magnificent!"
 }
